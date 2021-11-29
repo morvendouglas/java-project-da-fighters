25da06076599dc3bdbb4656bea4c0ad4e745d3ec
Vinn13-B <v.bennis@icloud.com>
2021-11-29 15:41:16 +0000

    computer turn and timer implemented, VB

--- src/components/FightScreen.js
+++ src/components/FightScreen.js
@@ -19,40 +19,84 @@ const FightScreen = ({ playerDa, computerDa }) => {
             setComputerHealth(health);
         } else {
             const health = computerDa.currentHealth -= getRandomNumber(18, 25)
-            setComputerHealth(health); 
+            setComputerHealth(health);
         }
+        computerTurn();
     }
 
     const handleAttack2Click = function () {
         const health = computerDa.currentHealth -= getRandomNumber(10, 35)
         setComputerHealth(health);
+        computerTurn();
+    }
+
+    const handleHeal = function () {
+        if (playerDa.daType === "SOFTDA") {
+            const health = playerDa.currentHealth += getRandomNumber(23, 25)
+            setPlayerHealth(health);
+        } else {
+            const health = playerDa.currentHealth += getRandomNumber(18, 25)
+            setPlayerHealth(health);
+        }
+        computerTurn();
+    }
+
+    const computerTurn = function () {
+        const Attack1 = function () {
+            if (computerDa.daType === "BUFFDA") {
+                const health = playerDa.currentHealth -= getRandomNumber(18, 30)
+                setPlayerHealth(health);
+            } else if (computerDa.daType === "AVERAGEDA") {
+                const health = playerDa.currentHealth -= getRandomNumber(23, 25)
+                setPlayerHealth(health);
+            } else {
+                const health = playerDa.currentHealth -= getRandomNumber(18, 25)
+                setPlayerHealth(health);
+            }
+        }
+        const Attack2 = function () {
+            const health = playerDa.currentHealth -= getRandomNumber(10, 35)
+            setPlayerHealth(health);
+        }
+        const Heal = function () {
+            if (computerDa.daType === "SOFTDA") {
+                const health = computerDa.currentHealth += getRandomNumber(23, 25)
+                setComputerHealth(health);
+            } else {
+                const health = computerDa.currentHealth += getRandomNumber(18, 25)
+                setComputerHealth(health);
+            }
+        }
+        const randomMove = [Attack1, Attack2, Heal]
+        const randomNumber = Math.floor(Math.random() * 3);
+        setTimeout(function() {
+            randomMove[randomNumber]()}, 1000);
     }
 
 
-
-
-return (
-    <>
-        <div>
-            {playerDa.name}
-            <ul>
-                <li><button onClick={handleAttack1Click}>{playerDa.attackOneName}</button></li>
-                <li><button onClick={handleAttack2Click}>{playerDa.attackTwoName}</button></li>
-                <li><button>{playerDa.specialName}</button></li>
-                <li>{playerHealth}</li>
-            </ul>
-        </div>
-        <div>
-            {computerDa.name}
-            <ul>
-                <li><button>{computerDa.attackOneName}</button></li>
-                <li><button>{computerDa.attackTwoName}</button></li>
-                <li><button>{computerDa.specialName}</button></li>
-                <li>{computerHealth}</li>
-            </ul>
-        </div>
-    </>
-)
+    return (
+        <>
+            <div>
+                {playerDa.name}
+                <ul>
+                    <li><button onClick={handleAttack1Click}>{playerDa.attackOneName}</button></li>
+                    <li><button onClick={handleAttack2Click}>{playerDa.attackTwoName}</button></li>
+                    <li><button onClick={handleHeal}>{playerDa.healName}</button></li>
+                    <li><button>{playerDa.specialName}</button></li>
+                    <li>{playerHealth}</li>
+                </ul>
+            </div>
+            <div>
+                {computerDa.name}
+                <ul>
+                    <li><button>{computerDa.attackOneName}</button></li>
+                    <li><button>{computerDa.attackTwoName}</button></li>
+                    <li><button>{computerDa.specialName}</button></li>
+                    <li>{computerHealth}</li>
+                </ul>
+            </div>
+        </>
+    )
 
 }
 
